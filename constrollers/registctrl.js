e1993b46f2e05fa9733571430ece235488ee8a1b
MrBai <jianwu@yoopay.cn>
2021-04-27 18:00:30 +0800

    bug

--- constrollers/registctrl.js
+++ constrollers/registctrl.js
@@ -1,7 +1,7 @@
 // 引入formidable
 var formidable = require("formidable")
 //链接需要的 model 
-var Account = require("../models/User.js")
+var User = require("../models/User.js")
 exports.ShowRegist = function (req,res) {
     res.render("regist")
 }
@@ -12,10 +12,10 @@ exports.doRegist = function (req,res) {
 //查询
 exports.checkRegist = function (req,res) {
     var form = new formidable.IncomingForm();
-    form.parse("req",function (err,fields,files) {  
-        //console.log(fields.email)
+    form.parse(req,function (err,fields,files) {  
+        console.log(fields)
         var email = fields.email;
-        Account.count({"email":email},function (err,count) {
+        User.count({"email":email},function (err,count) {
             console.log(count)
         })
     })
--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 var mongoose = require("mongoose");
 //创建表头   schema
-var AccountSchema = new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
     "email": String,
     "password":String
 });
 
 //创建 model，底层创建构造函数
-var Account = mongoose.model("Account",AccountSchema);
+var User = mongoose.model("User",UserSchema);
 //暴露
-module.exports = Account
+module.exports = User
